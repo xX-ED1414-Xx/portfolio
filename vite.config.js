46e6dbbb3266d691a52c2a17b0f306b690e83055
xX-ED1414-Xx <ditangeorgeedward@gmail.com>
2026-05-13 11:26:06 +0800

    WORK ALREADY PLEASE

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [tailwindcss()],
+  base: '/portfolio/',
+  plugins: [
+    tailwindcss(),
+  ],
 })
